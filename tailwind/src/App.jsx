c61084f1f5f4305deaac5028053fc881dc65059b
AlexCFrost <siva.ganesh.c18@gmail.com>
2024-04-03 15:39:30 +0530

    Completed

---
+++ tailwind/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Alert from './Components/Alert'
+import Content from './Components/Content'
+import Footer from './Components/Footer'
+import Navbar from './Components/NavBar'
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Alert/>
+      <Content/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
